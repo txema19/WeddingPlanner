be1b545269303f918b14fc3ffbd89994ca488917
ABlazquezM <andrea.blazquez.martin@gmail.com>
2024-01-13 12:28:02 +0100

    Creación del DTO Servicio

---
+++ src/main/java/gestion/wedding/model/ServicioDTO.java
@@ -0,0 +1,19 @@
+package gestion.wedding.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServicioDTO {
+	
+	private Integer id_servicio;
+	private String nombre;
+	private Double precio;
+	private Integer id_curso;
+	
+
+}
